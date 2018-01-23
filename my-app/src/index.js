6fdada475e42e9f5a23c06556354e99e48e1d6ae
Sebastian Hemmilä <sebastian.hemmil01@gmail.com>
2018-01-23 11:49:58 +0200

    finished tasks 1.3-1.5

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,19 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    const kurssi = 'Half Stack -sovelluskehitys'
-    const osa1 = 'Reactin perusteet'
-    const tehtavia1 = 10
-    const osa2 = 'Tiedonvälitys propseilla'
-    const tehtavia2 = 7
-    const osa3 = 'Komponenttien tila'
-    const tehtavia3 = 14
-
+    const kurssi = {
+        nimi: 'Half Stack -sovelluskehitys',
+        osat: [{
+            nimi: 'Reactin perusteet',
+            tehtavia: 10
+        }, {
+            nimi: 'Tiedonvälitys propseilla',
+            tehtavia: 7
+        }, {
+            nimi: 'Komponenttien tila',
+            tehtavia: 14
+        }
+        ]
+    }
     return (
         <div>
-            <Otsikko kurssi={kurssi} />
-            <Sisalto osa1={osa1} osa2={osa2} osa3={osa3} tehtavia1={tehtavia1} tehtavia2={tehtavia2} tehtavia3={tehtavia3} />
-            <Yhteensa tehtavia1={tehtavia1} tehtavia2={tehtavia2} tehtavia3={tehtavia3} />
+            <Otsikko kurssi={kurssi.nimi} />
+            <Sisalto osat={kurssi.osat} />
+            <Yhteensa osat={kurssi.osat} />
         </div>
     )
 }
@@ -22,7 +28,7 @@ const App = () => {
 const Yhteensa = (props) => {
     return (
         <div>
-            <p>yhteensä {props.tehtavia1 + props.tehtavia2 + props.tehtavia3} tehtävää</p>
+            <p>yhteensä {props.osat[0].tehtavia + props.osat[1].tehtavia + props.osat[2].tehtavia} tehtävää</p>
         </div>
     )
 }
@@ -38,9 +44,9 @@ const Otsikko = (props) => {
 const Sisalto = (props) => {
     return (
         <div>
-            <Osa osa={props.osa1} tehtavia={props.tehtavia1} />
-            <Osa osa={props.osa2} tehtavia={props.tehtavia2} />
-            <Osa osa={props.osa3} tehtavia={props.tehtavia3} />
+            <Osa osa={props.osat[0].nimi} tehtavia={props.osat[0].tehtavia} />
+            <Osa osa={props.osat[1].nimi} tehtavia={props.osat[1].tehtavia} />
+            <Osa osa={props.osat[2].nimi} tehtavia={props.osat[2].tehtavia} />
         </div>
     )
 }
